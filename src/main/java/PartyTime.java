80cfa938b01080663a72f873e5ca95deac0de8d9
Jackson Meyer <jackson.meyer7@gmail.com>
2016-09-10 13:15:31 -0700

    add more tests and java

--- src/main/java/PartyTime.java
+++ src/main/java/PartyTime.java
@@ -2,19 +2,20 @@
 
 public class PartyTime {
   private String mType;
-  private String mLocation;
-  private String mFood;
-  private String mDrink;
   private int mPeople;
   private int mHours;
+  private String mFood;
+  private String mDrink;
+  private String mLocation;
 
-  public PartyTime(String type, String location, String food, String drink, int people, int hours) {
+
+  public PartyTime(String type, int people, int hours , String food, String drink, String location) {
     mType = type;
-    mLocation = location;
-    mFood = food;
-    mDrink = drink;
     mPeople = people;
     mHours = hours;
+    mFood = food;
+    mDrink = drink;
+    mLocation = location;
   }
 
   public boolean isWedding(String weddingResponse) {
@@ -24,12 +25,21 @@ public class PartyTime {
       return false;
     }
   }
-  public int getBaseCostPerPerson(Integer mpeople) {
+  public int getBaseCostForPeople(Integer mpeople) {
       return (mpeople * 10); /* $10 base cost per person*/
     }
 
-  // public int getBaseCostPerPerson(Integer mpeople) {
-  //     return (mpeople * 10); /* $10 base cost per person*/
-  //   }
+  public int getNumberOfHours(Integer mhours) {
+      return (mhours * 2); /* there is a fee per hour for different venues*/
+    }
+
+  public int whichFoodPackage (String foodResponse) {
+    mPeople = 100;
+    if (foodResponse == "panda") {
+      return (mPeople * 15);
+    } else if (foodResponse == "fogoDeChao") {
+      return (mPeople * 50);
+    } else return mPeople * 10;
 
   }
+}
--- src/test/java/PartyTimeTest.java
+++ src/test/java/PartyTimeTest.java
@@ -7,29 +7,35 @@ public class PartyTimeTest {
 
   @Test
     public void NewPartyTime_instantiates_correctly(){
-      PartyTime testPartyTime = new PartyTime("a", "a", "a", "c", 1992, 100);
+      PartyTime testPartyTime = new PartyTime("a", 1992, 100, "a", "a", "c");
       boolean outcome = true;
       assertEquals(outcome, testPartyTime instanceof PartyTime);
     }
 
   @Test
     public void NewPartyTime_determinesIfItIsAWeddingOrNot_true(){
-      PartyTime testPartyTime = new PartyTime("a", "a", "a", "c", 1992, 100);
+      PartyTime testPartyTime = new PartyTime("a", 1992, 100, "a", "a", "c");
       boolean outcome = true;
       assertEquals(outcome, testPartyTime.isWedding("yes"));
     }
 
     @Test
       public void NewPartyTime_CostPerPerson_1000(){
-        PartyTime testPartyTime = new PartyTime("a", "a", "a", "c", 1992, 100);
+        PartyTime testPartyTime = new PartyTime("a", 1992, 100, "a", "a", "c");
         int outcome = 1000;
-        assertEquals(outcome, testPartyTime.getBaseCostPerPerson(100));
+        assertEquals(outcome, testPartyTime.getBaseCostForPeople(100));
       }
 
-      // @Test
-      //   public void NewPartyTime_determinesIfItIsAWeddingOrNot_true(){
-      //     PartyTime testPartyTime = new PartyTime("a", "a", "a", "c", 1992, 100);
-      //     boolean outcome = true;
-      //     assertEquals(outcome, testPartyTime.isWedding("yes"));
-      //   }
+    @Test
+      public void NewPartyTime_CalculatesHourlyFee_8(){
+        PartyTime testPartyTime = new PartyTime("a", 1992, 100, "a", "a", "c");
+        int outcome = 8;
+        assertEquals(outcome, testPartyTime.getNumberOfHours(4));
+      }
+    @Test
+      public void NewPartyTime_calculateFoodPackageCost_fogoDeChao(){
+        PartyTime testPartyTime = new PartyTime("a", 100, 4, "panda", "a", "c");
+        int outcome = 5000;
+        assertEquals(outcome, testPartyTime.whichFoodPackage("fogoDeChao"));
+      }
   }
